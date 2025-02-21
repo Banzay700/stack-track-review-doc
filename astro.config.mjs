24b999964b59679020cbc2ac81d62dc35fa30be4
Oleksandr Lotko <lotkoolexandr@gmail.com>
2025-02-21 13:54:51 +0200

    start

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Stack-track Review',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [{ label: 'Example Guide', slug: 'guides/example' }],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+        {
+          label: 'Typescript',
+          items: [
+            { label: 'Base Check', slug: 'typescript-errors/base-check' },
+          ],
+        },
+      ],
+    }),
+  ],
+})
